4414868c44a9c59838d234178927f2c066a10199
Peyton Meredith <peymeredith@gmail.com>
2023-10-04 20:01:54 -0500

    info flow lab working but fails tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header"
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -8,15 +9,12 @@ function App() {
   function onDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+  const buttonText = isDarkMode ? "Dark Mode" : "Light Mode"
+  // console.log(buttonText)
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      <Header onDarkModeClick={onDarkModeClick} buttonText = {buttonText} />
       <ShoppingList items={itemData} />
     </div>
   );
